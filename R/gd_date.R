b163ae3bb780731cadffdd51ce2f1069fcb9f900
Albert Cobos <acoboscarbo@gmail.com>
2018-04-07 12:27:10 +0200

    changes param description and examples

--- R/gd_date.R
+++ R/gd_date.R
@@ -2,25 +2,25 @@
 #'
 #' Describes a date variable by computing its range.
 #'
-#' @param x The date variable to be described (\code{c("Date", "POSIXlt",
-#' "POSIXct", "POSIXt")}).
+#' @param x The date variable to be described. Should be an object of one of the 
+#' following classes: \code{c("Date", "POSIXlt", "POSIXct", "POSIXt")}.
 #' @param date_format Optional. If specified, should ba a valid date format. See
-#' the Details section in \code{\link{strptime}} for more details.
+#' the Details section in \code{\link[base]{?strptime}} for more details.
 #'
 #' @return A \code{dataframe} with three columns: \code{Variable}, \code{Key},
 #' \code{Value}.
 #'
 #' @examples
-#' # Create example data
+#' # Example data
 #' inclusion_date <- as.Date(c("2017-01-01", "2017-03-25", "2018-01-31"))
 #'
 #' # Default date_format
 #' gd_date(inclusion_date)
 #'
-#' # Month as number, and day with leading space for single digit numbers
+#' # Month as number, and day without leading space for single digit numbers
 #' gd_date(inclusion_date, date_format="%e-%m-%y")
 #'
-#' # Four-digit year
+#' # Four-digits year
 #' gd_date(inclusion_date, date_format="%e-%m-%Y")
 #'
 #'
